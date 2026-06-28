54c3da67144d4a019637639a8d8cb3be1e388744
yba9285 <108516941+yba9285@users.noreply.github.com>
2026-06-28 21:33:49 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -296,7 +296,7 @@ Nᴀᴍᴇ : {}
 
 Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
     
-    CAPTION = """<b><a href="https://t.me/mzbotz">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a href="https://t.me/mzbotz">[ MZBOTZ ]</a></b>"""
+    CAPTION = """<b><a href="https://t.me/mzmoviiez">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a href="https://t.me/mzbotz">[ MZBOTZ ]</a></b>"""
 
     
     MOVIE_UPDATE_NOTIFY_TXT = """
